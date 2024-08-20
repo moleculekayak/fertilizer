a2c811f39c1bf549812037fef53eeb387922cbf8
milkers69 <milkdawg69420@proton.me>
2024-08-20 15:16:04 -0500

    remove accidental comment

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def __verify_api_keys(config):
   # This will perform a lookup with the API and raise if there was a failure.
   # Also caches the announce URL for future use which is a nice bonus
   red_api.announce_url
-  #ops_api.announce_url
+  ops_api.announce_url
 
   return red_api, ops_api
 
