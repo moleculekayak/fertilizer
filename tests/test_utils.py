24479d0341ce3fd94ec20fe71d70e6c2fad70c50
moleculekayak <moleculekayak@proton.me>
2024-10-03 10:10:42 -0700

    [Bugfix] Fixed URL joins for Windows machines (#22)
    * Fixed path joins for windows machines
    
    * improved tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
 from .helpers import SetupTeardown
 
-from src.utils import flatten
+from src.utils import flatten, url_join
 
 
 class TestFlatten(SetupTeardown):
@@ -9,3 +9,25 @@ class TestFlatten(SetupTeardown):
 
   def test_returns_already_flat_list(self):
     assert flatten([1, 2, 3]) == [1, 2, 3]
+
+
+class TestUrlJoin(SetupTeardown):
+  def test_joins_paths(self):
+    path = url_join("/api", "v1", "foo")
+
+    assert path == "api/v1/foo"
+
+  def test_joins_paths_when_some_have_leading_trailing_slash(self):
+    path = url_join("/api/", "/v1/", "foo/")
+
+    assert path == "api/v1/foo"
+
+  def test_joins_a_full_uri(self):
+    path = url_join("https://api.example.com/", "/v1", "foo")
+
+    assert path == "https://api.example.com/v1/foo"
+
+  def test_strips_bare_slashes(self):
+    path = url_join("https://api.example.com/", "/", "/v1/", "/foo/", "/")
+
+    assert path == "https://api.example.com/v1/foo"
